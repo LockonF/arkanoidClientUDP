83f2c8ce81dd198275fd763bd36777711dd1ec92
Daniel Franco <zurielovsko@gmail.com>
2015-07-14 17:55:56 -0500

    Initial Commit

---
+++ Arkanoid_Message_Client/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Arkanoid_Message_Client
+//
+//  Created by Daniel Franco on 7/14/15.
+//  Copyright (c) 2015 Daniel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
